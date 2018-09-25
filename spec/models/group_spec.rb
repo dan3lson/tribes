48066a8ae9a5712f34b56bf94a5ac3de37df8406
Danelson Rosa <git.danelson@gmail.com>
2018-09-25 17:28:19 -0400

    Check to see if a group has any users, is full, and its number of users.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,58 @@ RSpec.describe Group, type: :model do
   it { should belong_to(:tribe) }
   it { should have_many(:users) }
 	it { should validate_presence_of(:name) }
+
+  describe "#has_users?" do
+    it "returns true" do
+      group = FactoryBot.create(:user).group
+
+      expect(group).to be_has_users
+    end
+
+    it "returns false" do
+      group = FactoryBot.create(:group)
+
+      expect(group).not_to be_has_users
+    end
+  end
+
+  describe "#is_full?" do
+    it "returns true, for 99 users" do
+      group = FactoryBot.create(:group)
+
+      99.times do
+        FactoryBot.create(:user, group: group)
+      end
+
+      expect(group).to be_is_full
+    end
+
+    it "returns false, for 98 users" do
+      group = FactoryBot.create(:group)
+
+      98.times do
+        FactoryBot.create(:user, group: group)
+      end
+
+      expect(group).not_to be_is_full
+    end
+  end
+
+  describe "#num_users" do
+    it "returns integer for 1 user" do
+      group = FactoryBot.create(:group)
+
+      99.times do
+        FactoryBot.create(:user, group: group)
+      end
+
+      expect(group.num_users).to eq(99)
+    end
+
+    it "returns integer for 99 users" do
+      group = FactoryBot.create(:user).group
+
+      expect(group.num_users).to eq(1)
+    end
+  end
 end
